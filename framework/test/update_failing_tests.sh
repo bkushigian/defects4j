6a57428b1d8e645434621ffb94029c303535d047
bkushigian <bkushigian@gmail.com>
2020-03-05 09:46:14 -0800

    Cleaned up update_failing_test.sh

--- framework/test/update_failing_tests.sh
+++ framework/test/update_failing_tests.sh
@@ -2,15 +2,16 @@
 
 ################################################################################
 # This script updates the failing tests recorded by defects4j projects to
-# reflect Java 8-related failures. To do this, it runs `defects4j test` on
-# each fixed version of each problem and updates d4j's failing test database
-# respectively.
+# reflect new Java failures. To do this, it runs `defects4j test` on each fixed
+# version of each problem and updates d4j's failing test database respectively.
 #
 # This expects GNU parallel to be installed.
 #
-# USAGE: update_failing_tests.sh JAVA_HOME
+# USAGE: update_failing_tests.sh [JAVA_HOME]
 #        JAVA_HOME: the location of the Java install (such as
-#                   /usr/lib/jvm/openjdk-1.8.xxx)
+#                   /usr/lib/jvm/openjdk-1.8.xxx). This should either be
+#                   provided by command line or be provided via an environment
+#                   variable.
 ################################################################################
 
 source test.include
@@ -18,20 +19,24 @@ source test.include
 usage() {
     if [ "$1" ]
     then
-        printf "\033[31;1m$1\033[0m\n"
+        echo "$1"
     fi
 
-    printf "\033[1musage:\033[0m $0 java8-home\n"
+    printf "usage: %s java8-home\n"  "$0"
     exit 1
 }
 
-if [ -z $JAVA_HOME ]
+if [ -z "$JAVA_HOME" ]
 then
     JAVA_HOME=$1
 fi
-if [ -z $JAVA_HOME ]
+if [ -z "$JAVA_HOME" ]
 then
-    usage "No java_home provided"
+    usage "No JAVA_HOME provided"
+fi
+if [ ! -e "$JAVA_HOME" ] || [ ! -e "$JAVA_HOME/bin/javac" ]
+then
+  usage "JAVA_HOME doesn't exist: $JAVA_HOME"
 fi
 
 export PATH="$JAVA_HOME/bin:$PATH"
@@ -40,55 +45,50 @@ export JAVA_HOME
 # All projects
 projects=(Chart Closure Lang Math Mockito Time)
 
-export ANSI_ERROR="\033[91;1m"
-export ANSI_INFO="\033[94;1m"
-export ANSI_SUCCESS="\033[92;1m"
-export ANSI_CLEAR="\033[0m"
-export ANSI_GREEN="\033[32m"
-export ANSI_BANNER=$ANSI_SUCCESS
-
 echo "Running update_failing_tests.sh with JAVA_HOME=$JAVA_HOME"
 
+################################################################################
+# dtstring: Get the current time and date as a string
+################################################################################
 function dtstring {
     date "+%T %D"
 }
 
-# Pretty print the log to stdout. Accepts an ansi code to prepend
+################################################################################
+# Print the log to stdout as well as logging to file
+################################################################################
 function pplog {
-    message="$1"
-    code="$2"
-
-    log "[$(date "+%T")] $message"
-    printf "${code}${message}$ANSI_CLEAR\n"
+    log "[$(date "+%T")] $1"
+    echo "$1"
 }
 
-function loginfo {
-    pplog "$1" $ANSI_INFO
-}
-
-function logerr {
-    pplog "$1" $ANSI_ERROR
-}
-
-function logsucc {
-    pplog "$1" $ANSI_SUCCESS
-}
-
+################################################################################
+# Log a banner: --- My Message ---
+################################################################################
 function logbanner {
-    message="$1"
-
-    log "$message"
-    printf "${ANSI_BANNER}--- ${message} ---$ANSI_CLEAR\n"
+    log "$1"
+    echo "  --- $1 ---"
 }
 
+################################################################################
+# lookup_hash_name: lookup hashes for buggy and fixed commits in the
+# `framework/projects/$pid/commit-db` database. These are stored in global
+# variables BUGGY_COMMIT_HASH and FIXED_COMMIT_HASH.
+#
+# Arguments:
+#    1. pid: the project id that we care about (i.e., "Chart", "Math", etc)
+#    2. bid: the bug id that we care about (i.e., "1", "2", etc)
+#
+# Returns: commit hashes of buggy and fixed versions of the program in global
+# variables BUGGY_COMMIT_HASH and FIXED_COMMIT_HASH
+################################################################################
 function lookup_hash_name {
     pid=$1
-    v=$2
-    work_dir=$3
-    BUGGY_COMMIT_HASH=`sed "${v}q;d" "$BASE_DIR/framework/projects/$pid/commit-db" | cut -d, -f2`
-    FIXED_COMMIT_HASH=`sed "${v}q;d" "$BASE_DIR/framework/projects/$pid/commit-db" | cut -d, -f3`
-    pplog "Commit hash for $pid-$v (buggy): $BUGGY_COMMIT_HASH" $ANSI_GREEN
-    pplog "Commit hash for $pid-$v (fixed): $FIXED_COMMIT_HASH" $ANSI_GREEN
+    bid=$2
+    BUGGY_COMMIT_HASH=$(sed "${bid}q;d" "$BASE_DIR/framework/projects/$pid/commit-db" | cut -d, -f2)
+    FIXED_COMMIT_HASH=$(sed "${bid}q;d" "$BASE_DIR/framework/projects/$pid/commit-db" | cut -d, -f3)
+    pplog "Commit hash for $pid-$bid (buggy): $BUGGY_COMMIT_HASH"
+    pplog "Commit hash for $pid-$bid (fixed): $FIXED_COMMIT_HASH"
 }
 
 function run_tests {
@@ -97,9 +97,8 @@ function run_tests {
     export -f die
     export -f pplog
     export -f logbanner
-    export -f logerr
-    export -f loginfo
-    export -f logsucc
+    export -f pplog
+    export -f pplog
     export -f lookup_hash_name
     export -f log
     export -f num_lines
@@ -109,87 +108,78 @@ function run_tests {
     export BASE_DIR
     export TMP_DIR
 
-    parallel   --jobs 16 --progress --bar  run_test_on_pid ::: "${projects[@]}"
+    parallel   --jobs 6 --progress --bar  run_tests_on_pid ::: "${projects[@]}"
 }
 
-function run_test_on_pid {
+function run_tests_on_pid {
     pid=$1
-    echo $pid
 
     # Create log file
-    script_name=$(echo $script | sed 's/\.sh$//')
-    LOG="$TEST_DIR/${script_name}$(printf '_%s_%s' $pid $$).log"
-    loginfo "JAVA_HOME: $JAVA_HOME"
+    script_name=$(echo "$script" | sed 's/\.sh$//')
+    LOG="$TEST_DIR/${script_name}$(printf '_%s_%s' "$pid" $$).log"
 
-    logbanner "Working on $pid"
+    logbanner "Running tests on $pid"
     # Reproduce all bugs (and log all results), regardless of whether errors occur
-    HALT_ON_ERROR=0
+    export HALT_ON_ERROR=0
 
-    # Get number of bugs in pid
-    num_bugs=$(num_lines $BASE_DIR/framework/projects/$pid/commit-db)
-    BUGS="$(seq 1 1 $num_bugs)"
+    # Compute $BUGS as a sequence of each bug id
+    num_bugs=$(num_lines "$BASE_DIR/framework/projects/$pid/commit-db")
+    BUGS="$(seq 1 1 "$num_bugs")"
 
     test_dir="$TMP_DIR/run_tests"
-    mkdir -p $test_dir
+    mkdir -p "$test_dir"
 
     work_dir="$test_dir/$pid"
     # Ensure a clean working directory
-    rm -rf $work_dir
+    rm -rf "$work_dir"
 
-    loginfo "logging to $LOG"
-    loginfo "num_bugs: $num_bugs"
-    loginfo "test_dir: $test_dir"
-    loginfo "work_dir: $work_dir"
+    pplog "logging to $LOG"
+    pplog "num_bugs: $num_bugs"
+    pplog "test_dir: $test_dir"
+    pplog "work_dir: $work_dir"
 
-    for bid in $(echo $BUGS); do
-        rm -rf $work_dir
+    for bid in $BUGS
+    do
+        rm -rf "$work_dir"
         vid="${bid}f"
         logbanner "Working on $pid-$vid ($bid of $num_bugs)"
-        loginfo "[+] checking out project: pid=$pid vid=$vid work_dir=$work_dir"
+        pplog "[+] checking out project: pid=$pid vid=$vid work_dir=$work_dir"
         defects4j checkout -p $pid -v "$vid" -w "$work_dir" || die "checkout: $pid-$vid"
-        loginfo "[+] compiling project: pid=$pid vid=$vid work_dir=$work_dir"
+        pplog "[+] compiling project: pid=$pid vid=$vid work_dir=$work_dir"
         defects4j compile -w "$work_dir" || die "compile: $pid-$vid"
-        loginfo "[+] running tests: pid=$pid vid=$vid work_dir=$work_dir"
+        pplog "[+] running tests: pid=$pid vid=$vid work_dir=$work_dir"
         defects4j test -w "$work_dir"    || die "run tests: $pid-$vid"
 
         triggers=$(num_triggers "$work_dir/failing_tests")
+        pplog "found $triggers triggering tests"
 
-        loginfo "found $triggers triggering tests"
         # If there are failing tests, then add them to the defects4j repo
-        if [ $triggers -ne 0 ] ; then
-            lookup_hash_name $pid $bid $work_dir
+        if [ "$triggers" -ne 0 ] ; then
+            lookup_hash_name "$pid" "$bid" "$work_dir"
 
             # Echo to failing_tests
             src="$work_dir/failing_tests"
             trg="$BASE_DIR/framework/projects/$pid/failing_tests/$FIXED_COMMIT_HASH"
-            loginfo "updating failing tests: $src >> $trg"
+            pplog "updating failing tests: $src >> $trg"
             cat "$src" >> "$trg"
-            rm -rf $work_dir
+            rm -rf "$work_dir"
 
             # Rerun to sanity check that this worked
-            loginfo "rerunning defects4j test -w $work_dir with appended failing tests"
-            loginfo "[+] checking out project: pid=$pid vid=$vid work_dir=$work_dir"
+            pplog "rerunning defects4j test -w $work_dir with appended failing tests"
+            pplog "[+] checking out project: pid=$pid vid=$vid work_dir=$work_dir"
             defects4j checkout -p $pid -v "$vid" -w "$work_dir" || die "checkout: $pid-$vid"
-            loginfo "[+] compiling project: pid=$pid vid=$vid work_dir=$work_dir"
+            pplog "[+] compiling project: pid=$pid vid=$vid work_dir=$work_dir"
             defects4j compile -w "$work_dir" || die "compile: $pid-$vid"
-            loginfo "[+] running tests: pid=$pid vid=$vid work_dir=$work_dir"
+            pplog "[+] running tests: pid=$pid vid=$vid work_dir=$work_dir"
             defects4j test -w "$work_dir"    || die "run tests: $pid-$vid"
 
             triggers=$(num_triggers "$work_dir/failing_tests")
-            if [ $triggers -ne 0 ] ; then
+            if [ "$triggers" -ne 0 ] ; then
                 logerr "[!!!] Tests still failing after updating project failing tests"
                 return 1
             fi
 
-            loginfo "   Tests succeeded"
-#            for name in "all_tests" "failing_tests"
-#            do
-#                src="$work_dir/$name"
-#                trg="$dirname/$name"
-#                loginfo "   Moving $src--->$trg"
-#                mv "$src" "$trg"
-#            done
-
+            pplog "   Tests succeeded"
         fi
 
 
